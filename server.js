c1c749bdbaae53f31454e6ed50da3be294137aff
ANASS OULED BEN TAHAR <57966523+ANASS99T@users.noreply.github.com>
2021-05-22 16:52:40 +0100

    fix error

--- server.js
+++ server.js
@@ -1,43 +1,53 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const User = require('./user');
+const bcrypt = require('bcryptjs');
 
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const app = express()
-const User = require('./modele/user')
-const bcrypt = require('bcryptjs')
+mongoose.connect('mongodb://localhost:27017/login-app-bd', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+// app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'htmlpage.html'));
+});
 
-mongoose.connect('mongodb://localhost:27017/login-app-bd' , {
-	useNewUrlParser:true,
-	useUnifiedTopology:true,
-	useCreateIndex: true
-})
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json())
+app.get('/login',function (req, res) {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
 
-app.post('http://localhost:9999/api/register', async (req, res) =>
-{
-	console.log(req.body)
+app.post('/api/register', async (req, res) => {
+  console.log(req.body);
 
-	const {username, password: plainTextPassword }= req.body
+  const { username, password: plainTextPassword } = req.body;
 
-	const password = await bcrypt.hash(plainTextPassword, 10)
+  const password = await bcrypt.hash(plainTextPassword, 10);
 
-     try{
-          await User.create({
-          	username,
-          	password
-          })
-          console.log('User created successfully:',response)
-     }
-     catch (error){
-       console.log(error)
-       return res.json({status: 'error'})
-     }
-	res.json({status: 'ok'})
-})
+  try {
+    await User.create({
+      username,
+      password,
+    });
+    // console.log('User created successfully:', res);
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: 'error' });
+  }
+//   res.json({ status: 'ok');
+ 	res.redirect('/login');
+});
 
-app.listen(9999, () => {
-	console.log('Server up at 9999')
-})
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000');
+});
