02e74dbf66c8b623ad1c8accc198bad1959255bf
guisser <guisserzinebmb@gmail.com>
2021-05-23 03:12:20 +0100

    register and login page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 const User = require('./user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET= 'kjuiurozimqkmqsk@mù$$^$ù^$^mlklkjddqhggjhgqsjhgvbcv'
 
 mongoose.connect('mongodb://localhost:27017/login-app-bd', {
   useNewUrlParser: true,
@@ -12,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/login-app-bd', {
   useCreateIndex: true,
 });
 // app.use('/', express.static(path.join(__dirname, 'static')));
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -27,27 +31,79 @@ app.get('/login',function (req, res) {
   res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-app.post('/api/register', async (req, res) => {
-  console.log(req.body);
+app.get('/loginP',function (req, res) {
+  res.sendFile(path.join(__dirname, 'loginPage.html'));
+});
 
+app.post('/api/loginPage', async(req,res) =>
+{
+
+  const {username, password } = req.body
+  const user = await User.findOne({username}).lean()
+
+  if(!user)
+  {
+    return res.json({status : 'error', error:'Invalid user/username/password'})
+  }
+
+  if(await bcrypt.compare(password, user.password)){
+
+
+       const token = jwt.sign(
+        {id: user._id,
+        username: user.username
+
+      }, JWT_SECRET
+      )
+
+        return res.json({status : 'ok', date : token})
+
+
+  }
+
+  res.json({status : 'error', error:'Invalid user/username/password'})
+
+});
+
+app.post('/api/register', async (req, res) => {
   const { username, password: plainTextPassword } = req.body;
 
+
+if(!username || typeof username !== 'string'){
+    return res.json({ status : 'error' , error : 'invalid username'})
+  }
+
+  if(!plainTextPassword || typeof plainTextPassword !== 'string'){
+    return res.json({status : 'error' , error : 'invalid password'})
+  }
+  if(plainTextPassword.length < 10)
+  {
+    return res.json({
+      status: 'error',
+      error : 'password too small should be atleast 10 characters'
+    })
+  }
   const password = await bcrypt.hash(plainTextPassword, 10);
 
   try {
-    await User.create({
+    const response = await User.create({
       username,
       password,
     });
     // console.log('User created successfully:', res);
-  } catch (error) {
-    console.log(error);
-    return res.json({ status: 'error' });
   }
-//   res.json({ status: 'ok');
+   catch (error) {
+    if(error.code === 11000)
+      {
+        return res.json({status: 'error',error:'Username already exist'})
+
+      }
+      throw error
+  }
+  // res.json({ status: 'ok');
  	res.redirect('/login');
 });
 
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000');
+app.listen(9999, () => {
+  console.log('Server started at http://localhost:9999');
 });
